a4c0ddd94907609cc34a87a39c2f1e3159990173
BuildTools <tibo.louagie@student.howest.be>
2020-12-05 15:53:34 +0100

    Task 3 first push

---
+++ Taak3/dhcp_ranges.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+NAME=${1?Error: no name given}
+
+echo $NAME
+
+
+#DHCP
+#Package: isc-dhcp-server
+#ISC = Internet Systems Consortium -https://www.isc.org/mission/
+#----------------------------------------------------------------
+
+port="67"
+IPTABLES_dest="192.168.100.40"
+ens_interfaces="ens34"
+
+sudo apt install isc-dhcp-server 
+
+#System-wide host-specific configuration file ? /etc/default/isc-dhcp-server
+#Example config
+
+#/default/isc-dhcp-server :
+INTERFACESv4=$ens_interfaces
+
+#/etc/dhcp/dhcpd.conf :
+configfile=${cat dhcp-ranges.txt}
+
+echo "$configfile" >> /etc/dhcp/dhcpd.conf
+
+#Allow DHCP server in IPTABLES
+sudo iptables -A INPUT -p UDP -d $IPTABLES_dest -m
+state --state NEW -m udp --dport=$port -j ACCEPT
+
+# Restart(reload) ISC-DHCP server
+sudo systemctl restart isc-dhcp-server.service
